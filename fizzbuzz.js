18334b7e3f9a26e1a906f9db70f0537a9083e998
chitlee <chit.lee@softwire.com>
2023-07-03 16:17:59 +0100

    only 3 and 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,21 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
 
-    // Put your code here...
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            if (i % 5 === 0) {
+                console.log("FizzBuzz")
+            } else {
+                console.log("Fizz")
+            }
+        } else {
+            if (i % 5 === 0) {
+                console.log("Buzz")
+            } else {
+                console.log(i)
+            }
+        }
+    }
 
 }
 
